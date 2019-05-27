ba919c383e03e56dd2d4e754e14c88b0821e7669
yoo <fkdn1234@naver.com>
2019-05-27 21:38:19 +0900

    App update

--- src/pages/oj/App.vue
+++ src/pages/oj/App.vue
@@ -7,7 +7,7 @@
       </transition>
       <div class="footer">
         <p v-html="website.website_footer"></p>
-        <p>Copyright &copy; 2018. <a href="https://blog.naver.com/ndb796">나동빈 Co. All right reserved.</a>
+        <p>Copyright &copy; 2018. <a href="https://www.konkuk.ac.kr">건국대학교Co. All right reserved.</a>
           <span v-if="version">&nbsp; Version: {{ version }}</span>
         </p>
       </div>
